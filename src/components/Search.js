255351281369437796980b4e5f91a7880ce0fb56
omayma ahmad <omayma.ahmad25@gmail.com>
2021-08-25 20:07:26 +0100

    added URL validation'

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,44 @@
 import React, {useState} from 'react';
 import { TextField, Button, LinearProgress } from '@material-ui/core'
-import shrtcode from '../api/shrtcode'
 import axios from 'axios'; 
+import styled from 'styled-components';
 
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+const HTTP_URL_VALIDATOR_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
 const Search = () => {
     const[link, setLink] = useState('');
-    const [short, setShort] = useState('')
+    const [short, setShort] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+     
+    const validateURL = (string) => 
+    {
+        return string.match(HTTP_URL_VALIDATOR_REGEX)
+    }
     
-    
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(link);
-        getLink();
-
+        if (validateURL(link)) {
+           getLink();
+        setLink('');
+        setIsLoading(!isLoading) 
+        } else {
+            setShort('please input valid url')
+        }
     };
 
     const getLink = async () => {
         try{
         const {data} = await axios
         .get(`https://api.shrtco.de/v2/shorten?url=${link}`)
-       setShort(data.result.short_link)
+       setShort(data.result.short_link);
+       setIsLoading(false)
         }
         catch(error){
             console.log(error.message)
@@ -30,17 +48,26 @@ const Search = () => {
     
     return (
         <>
-            <form onSumbit={(e) => handleSubmit(e)} style={{ display: 'flex', flexDirection: 'column'}}>
+            <StyledForm onSumbit={(e) => handleSubmit(e)}>
                 <TextField style={{ marginBottom: '20px'}}  
-                label="Input your link"
+                label="Link"
                 variant="outlined"
                 value={link}
                 onChange={(e) => setLink(e.target.value)}
                 />
-                <Button onClick={(e) => handleSubmit(e)}variant="contained" color="primary">
+                {!isLoading && (
+                  <Button 
+                style={{marginBottom: '20px'}}
+                onClick={(e) => handleSubmit(e)}
+                variant="contained" 
+                color="primary">
                     Submit
-                </Button>
-            </form>
+                </Button>  
+                ) }
+
+                {isLoading && <LinearProgress />}
+                
+            </StyledForm>
             <div>
                 {short !== '' ? <a target='_blank' rel='noreferrer' href={link}>{short}</a> : <p>your short link will appear here</p>}
                 
